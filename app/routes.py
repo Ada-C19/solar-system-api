66db8adb105a3089718e5d5e6e2d40ef178ca9b6
Kira Sidhu <kira.kaur.sidhu@gmail.com>
2023-04-20 11:22:52 -0700

    Define planet in routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,19 @@
 from flask import Blueprint
 
+class Planet:
+    def __init__(self, id, name, description):
+        self.id = id
+        self.name = name
+        self.description = description
+
+mercury = Planet(1, "Mercury", "smallest planet, and hot")
+venus = Planet(2, "Venus", "small, the hottest")
+earth = Planet(3, "Earth", "medium, green, lush, home planet, in danger")
+mars = Planet(4, "Mars", "red, no water, also in danger")
+jupiter = Planet(5, "Jupiter", "largest, many moons")
+saturn = Planet(6, "Saturn", "cool rings, large")
+uranus = Planet(8, "Uranus", "large, funny name")
+neptune = Planet(7, "Neptune", "large, blue, cold")
+pluto = Planet(8, "Pluto", "dwarf planet, very cold")
+
+planet_list = [mercury, venus, earth, mars, jupiter, saturn, uranus, neptune, pluto]
