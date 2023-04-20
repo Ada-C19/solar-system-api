dfa50123d8face4adb8ab648067a5a9888c5fa10
chickenandegg <tei.villanueva@gmail.com>
2023-04-20 11:40:31 -0700

    create a list of planets

---
+++ app/planets.py
@@ -0,0 +1,35 @@
+from flask import Blueprint
+
+class Planet:
+    def __init__(self, id, name, description, size):
+        self.id  = id
+        self.name = name
+        self.description = description
+        self.size = size
+
+
+planet1 = Planet(1, "Mercury", "It has an average distance of about 57 million kilometer from the sun", 4,879.4)
+
+planet2 = Planet(2, "Venus", "It has an average distance of about 108 million kilometer from the sun", 12,104)
+
+planet3 = Planet(
+    3, "Earth", "It has an average distance of about 150 million kilometer from the sun", 12,742)
+
+planet4 = Planet(
+    4, "Mars", "It has an average distance of about 228 million kilometer from the sun", 6,779)
+
+planet5 = Planet(
+    5, "Jupiter", "It has an average distance of about 779 million kilometer from the sun", 139,820)
+
+planet6 = Planet(
+    6, "Saturn", "It has an average distance of about 1.46 million kilometer from the sun", 116,460)
+
+planet7 = Planet(
+    1, "Uranus", "It has an average distance of about 2.88 million kilometer from the sun", 50,724)
+
+planet8 = Planet(
+    8, "Neptune", "It has an average distance of about 4.5 million kilometer from the sun", 49,244)
+
+
+planet_list = [planet1, planet2, planet3, planet4, planet5, planet6, planet7, planet8]
+
