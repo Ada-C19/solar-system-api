875405f5b644dcddb538b3fd035966e77ee0f87e
Charlie <charliethemushi@gmail.com>
2023-04-20 12:53:57 -0700

    fixed listed instances of planets

--- app/routes.py
+++ app/routes.py
@@ -8,16 +8,17 @@ class Planet():
         self.description = description
         self.place = place
 
-Mercury = Planet(1, "Mercury", "filler", "First")
-Venus = Planet(2, "Venus", "Filler", "Second")
-Earth = Planet (3, "Earth", "Filler", "Third")
-Mars = Planet(4, "Mars", "Filler", "Forth")
-Jupiter = Planet (5, "Jupiter", "Filler", "Fifth")
-Saturn= Planet (6, "Saturn", "Filler", "Sixth")
-Uranus= Planet (7, "Uranus", "Filler", "Seventh")
-Neptune = Planet(8, "Neptune", "Filler", "Eighth")
+Planets = [
+    Planet(1, "Mercury", "filler", "First"),
+    Planet(2, "Venus", "Filler", "Second"),
+    Planet(3, "Earth", "Filler", "Third"),
+    Planet(4, "Mars", "Filler", "Forth"),
+    Planet(5, "Jupiter", "Filler", "Fifth"),
+    Planet(6, "Saturn", "Filler", "Sixth"),
+    Planet(7, "Uranus", "Filler", "Seventh"),
+    Planet(8, "Neptune", "Filler", "Eighth")
+]
 
-Planets = [Mercury,Venus,Earth,Mars,Jupiter,Saturn,Uranus,Neptune]
 #...to get all existing planets, 
 # so that I can see a list of planets, 
 # with their id, name, description, 
