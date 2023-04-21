f445de9fac89fc26c113453899f984e23f6b9aec
nadia <reach.nadia.saria@gmail.com>
2023-04-21 11:30:14 -0700

    defines planet class

---
+++ app/planets.py
@@ -0,0 +1,9 @@
+# Define a Planet class with the attributes id, name, 
+# and description, and one additional attribute
+
+class Planet:
+    def __init__(self, id, name, description, mass):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.mass = mass 
