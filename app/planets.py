73993bb2e9616bd47e21a03dd869008f702b653a
nadia <reach.nadia.saria@gmail.com>
2023-04-21 11:44:11 -0700

    adds missing commas to list of planets

--- app/planets.py
+++ app/planets.py
@@ -10,13 +10,13 @@ class Planet:
 
 
 planets = [
-    Planet(1, "Mercury", "smallest planet", 176.0)
-    Planet(2, "Venus", "planet of love", 243.0)
-    Planet(3, "Earth", "home planet", 1.0)
-    Planet(4, "Mars", "red planet", 1.25)
-    Planet(5, "Jupiter", "largest planet", 0.42)
-    Planet(6, "Saturn", "ring planet", .45)
-    Planet(7, "Uranus", "coldest planet", .71)
-    Planet(8, "Neptune", "not visible to the naked eye", .67)
+    Planet(1, "Mercury", "smallest planet", 176.0),
+    Planet(2, "Venus", "planet of love", 243.0),
+    Planet(3, "Earth", "home planet", 1.0),
+    Planet(4, "Mars", "red planet", 1.25),
+    Planet(5, "Jupiter", "largest planet", 0.42),
+    Planet(6, "Saturn", "ring planet", .45),
+    Planet(7, "Uranus", "coldest planet", .71),
+    Planet(8, "Neptune", "not visible to the naked eye", .67),
     Planet(9, "Pluto", "unqualified planet", 6.375)
     ]
