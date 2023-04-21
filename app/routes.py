5bb6883a45ad75fd807210b94f80cf6bd8231efc
Sel <selenetenorio27@hotmail.com>
2023-04-21 12:12:01 -0700

    Libraries make_response and abort added to wave_02

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, abort, make_response
 
 class Planet:
     def __init__(self, id, name, description, color):
@@ -15,6 +15,22 @@ planets = [
 
 planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
 
+def validate_planet(planet_id):
+    #handle invalid planet_id, return 400
+    try:
+        planet_id = int(planet_id)
+    except:
+        abort(make_response({ "message":f"planet {planet_id} invalid"}, 400))
+
+    #search for planet_in in data, return planet
+    for planet in planets:
+        if planet.id == planet_id:
+            return planet
+
+    #return a 404 for non-existing planet
+    abort(make_response({"message":f"planet {planet_id} not found"}, 404))
+    
+
 @planets_bp.route("",methods=["GET"])
 def list_planets():
     planets_response = [vars(planet) for planet in planets]
@@ -23,18 +39,12 @@ def list_planets():
 
 @planets_bp.route("/<planet_id>", methods=["GET"])
 def single_planet(planet_id):
-    try:
-        planet_id = int(planet_id)
-    except: return { "message":f"planet {planet_id} invalid"}, 400
-    
-    for planet in planets:
-        if planet.id == planet_id:
-            return {
+    planet = validate_planet(planet_id)
+        
+    return {
                 "id":planet.id,
                 "name":planet.name,
                 "description":planet.description,
                 "color":planet.color
             }
-        
-    return { "message":f"planet {planet_id} not found"}, 404
     
