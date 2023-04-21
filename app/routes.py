476e5c5101a2f5f02554c564ae3d6967cdbe86e0
TombAnneX <109308405+AKTuminello@users.noreply.github.com>
2023-04-21 13:45:36 -0600

    add some flask imports and decorators

--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,9 @@
 from flask import Blueprint
 
+solar_system_bp = Blueprint('solar_system', __name__)
+
+
+@solar_system_bp.route('/solar_system', methods=['GET'])
+
+def get_solar_system():
+    return {"name": "Solar System"}
