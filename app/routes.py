149f21ced6f4fb73cb0bdb5e52bf75b7af94bf4e
Danica <danicas.edu@gmail.com>
2023-04-21 11:42:49 -0700

    Wave 1 commit

--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,17 @@
 from flask import Blueprint
 
+class Planet:
+    def __init__(self, id, name, description, color):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.color = color
+
+planets = [
+    Planet(1, "Earth", "habitable", "blue & green"),
+    Planet(2, "Venus", "hot", "yellowy orange"),
+    Planet(3, "Saturn", "beautiful ringlets", "light yellow"),
+    Planet(4, "Neptune", "furthest from the sun", "blue")
+]
+
+planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
