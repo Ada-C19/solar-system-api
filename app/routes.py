016b5a5bd7eebd5c85352c9315b395f3c880e298
Barbara <barbaraelisabethb@gmail.com>
2023-04-21 15:59:55 -0400

    Wave_01 - changes

--- app/routes.py
+++ app/routes.py
@@ -8,14 +8,14 @@ class Planet:
         self.position = position
 
 planets = [
-    Planet(1, "Mercury", "Closest to the sun and smallest", "#1")
-    Planet(2, "Venus", "The hottest planet of the Solar System", "#2") 
-    Planet(3, "Earth", "Seventy percent of its surface is cover with water", "#3") 
-    Planet(4, "Mars", "Known as Red Planet because of iron oxide on its surface", "#4")
-    Planet(5, "Jupiter", "The largest of the solar system, it's 2.5 times larger than all the other planets combined", "#5") 
-    Planet(6, "Saturn", "Known as a gas giant with seven ring systems surrounding it", "#6")
-    Planet(7 "Uranus", "It is the coldest planet of the Solar System with temperatures at around -224 degrees Celsius", "#7")
-    Planet(8 "Neptune", "Has the fasted wind speeds of any planet, reaching speeds of 2.160 km / 1.314 mi per hour", "#8")
+    Planet(1, "Mercury", "Closest to the sun and smallest", "#1"),
+    Planet(2, "Venus", "The hottest planet of the Solar System", "#2"), 
+    Planet(3, "Earth", "Seventy percent of its surface is cover with water", "#3"), 
+    Planet(4, "Mars", "Known as Red Planet because of iron oxide on its surface", "#4"),
+    Planet(5, "Jupiter", "The largest of the solar system, it's 2.5 times larger than all the other planets combined", "#5"), 
+    Planet(6, "Saturn", "Known as a gas giant with seven ring systems surrounding it", "#6"),
+    Planet(7, "Uranus", "It is the coldest planet of the Solar System with temperatures at around -224 degrees Celsius", "#7"),
+    Planet(8, "Neptune", "Has the fasted wind speeds of any planet, reaching speeds of 2.160 km / 1.314 mi per hour", "#8")
     ]
 
 bp = Blueprint("planets", __name__, url_prefix="/planets")
