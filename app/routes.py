6551fc0c048f4102b2e4c4d2db36e5cfeaa81c96
TombAnneX <109308405+AKTuminello@users.noreply.github.com>
2023-04-21 18:33:19 -0600

    "Completed through Planet class, need instances."

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,13 @@
 from flask import Blueprint
 
+class Planet:
+    def __init__(self, id, name, description, dista
+    nce_from_the_sun): #distance from the sun measured in AU--Astronmical Units
+        self.id = id
+        self.name = name
+        self.description = description
+        self.distance_from_the_sun = distance_from_the_sun
+
 solar_system_bp = Blueprint('solar_system', __name__)
 
 
