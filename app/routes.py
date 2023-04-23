34433c66b183a6ce3a6b3f20be9bea7a050f63ae
Alejandra <adom2128@gmail.com>
2023-04-23 09:11:46 -0600

    updates formatting and association

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,14 @@
 from flask import Blueprint, jsonify
 
+
 class Planet:
     def __init__(self, id, name, description, association):
         self.id = id
         self.name = name
         self.description = description
         self.association = association
-    
+
+
 planets = [
     Planet(1, "Mercury", "smallest planet", "Wednesday"),
     Planet(2, "Mars", "spicy, red one", "Tuesday"),
@@ -17,14 +19,15 @@ planets = [
 
 planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
 
-@planets_bp.route("",methods=["GET"])
+
+@planets_bp.route("", methods=["GET"])
 def handle_planets():
-    planets_list=[]
+    planets_list = []
     for planet in planets:
         planets_list.append(dict(
             id=planet.id,
             name=planet.name,
             description=planet.description,
-            weekday=planet.weekday
+            weekday=planet.association
         ))
     return jsonify(planets_list)
