9a138b25f971be92d894568880708cc0c0ac1aeb
Amber Shay <mashaad21@gmail.com>
2023-04-23 16:07:12 -0600

    working endpoint complete

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,9 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
+
 
 class Planet:
     """Create class Planet"""
+
     def __init__(self, id, name, description, type):
         """Constructor with class attributes"""
         self.id = id
@@ -9,11 +11,13 @@ class Planet:
         self.description = description
         self.type = type
 
+
 # Create a list of instances of class Planet for each planet in our solar system.
 planets = [
     Planet(1, "Mercury", "Smallest planet in the solar system. Its year is 88 days long. Closest to the Sun.", "Rocky/Terrestrial"),
     Planet(2, "Venus", "Hottest planet in the solar system. Has a thick, toxic atmosphere. Second planet from the Sun.", "Rocky/Terrestrial"),
-    Planet(3, "Earth", "Water world rich with life. Third rock from the Sun.", "Rocky/Terrestrial"),
+    Planet(3, "Earth", "Water world rich with life. Third rock from the Sun.",
+           "Rocky/Terrestrial"),
     Planet(4, "Mars", "Cold and desert-like; iron oxide makes it look red. Fourth planet from the Sun.", "Rocky/Terrestrial"),
     Planet(5, "Jupiter", "Largest planet in the solar system; home to the Great Red Spot. Fifth planet from the Sun.", "Gas giant"),
     Planet(6, "Saturn", "Known for its large and distinct planetary ring. Sixth planet from the Sun.", "Gas giant"),
@@ -22,3 +26,18 @@ planets = [
 ]
 
 planets_bp = Blueprint("planets_bp", __name__, url_prefix="/planets")
+
+
+@planets_bp.route("", methods=["GET"])
+def show_all_planets():
+    planets_response = []
+    for planet in planets:
+        planets_response.append(
+            {
+                "id": planet.id,
+                "name": planet.name,
+                "description": planet.description,
+                "type": planet.type,
+            }
+        )
+    return jsonify(planets_response)
