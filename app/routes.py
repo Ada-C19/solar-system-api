f9b18da5681026891387d130fe1bdb2bce4257cd
halimAremu <halimat3000@gmail.com>
2023-04-24 10:09:22 -0500

    defined handle all palnets route

--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,29 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
 
+class Planet:
+    def __init__(self, id, name, description, diameter):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.diameter = diameter
+ 
+planets = [Planet(1, 'Venus', 'The hottest planet',12104 ),
+           Planet(2,'Mercury', 'Has no natural satellite', 4879),
+           Planet(3,'Earth', 'The only planet where life is found', 12742 )
+     
+ ]
+
+planets_bp= Blueprint('planets', __name__,url_prefix='/planets')
+
+@planets_bp.route("", methods=['GET'])
+
+def handle_planets():
+    planet_records= []
+    for planet in planets:
+        planet_records.append(
+            {"id": planet.id,
+              "name": planet.name,
+              "description": planet.description,
+              "diameter": planet.diameter}
+        )
+    return jsonify(planet_records)
