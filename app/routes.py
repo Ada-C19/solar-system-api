9edb99aa5f7334378ce0563a3f9b6dfba3bde111
nadia <reach.nadia.saria@gmail.com>
2023-04-24 16:43:04 -0700

    adds to_dict function to planet class and deletes planets.py

--- app/routes.py
+++ app/routes.py
@@ -8,8 +8,15 @@ class Planet:
         self.id = id
         self.name = name
         self.description = description
-        self.solar_day = solar_day 
+        self.solar_day = solar_day
 
+    def to_dict(self):
+        return dict(
+            id=self.id,
+            name=self.name,
+            description=self.description,
+            solar_day=self.solar_day
+        )
 
 planets = [
     Planet(1, "Mercury", "smallest planet", 176.0),
@@ -22,73 +29,36 @@ planets = [
     Planet(8, "Neptune", "not visible to the naked eye", .67),
     Planet(9, "Pluto", "unqualified planet", 6.375)
     ]
-
-
-bp = Blueprint("planets". __name__, url_prefix="/planets")
-
-@bp.route("", methods=["GET"])
-def handle_planets():
-    results = []
-    for planet in planets:
-        results.append(dict(
-            id=planet.id,
-            name=planet.name,
-            description=planet.description,
-            solar_day=planet.solar_day
-        ))
-    return jsonify(results)
 
         # Wave 2
         # read one planet
         # return 400 for invalid id
         # return 404 for non existing planet
-
-def validate_planet(planet_id):
-    try:
-        planet_id = int(planet_id)
-    except:
-        abort(make_response({"message":f"book {planet_id} invalid"}, 400))
-
-    for planet in planets:
-        if planet.id == planet_id:
-            return planet
-
-    abort(make_response({"message":f"book {planet_id} not found"}, 404))
-
-
-planets = [
-    Planet(1, "Mercury", "smallest planet", 176.0),
-    Planet(2, "Venus", "planet of love", 243.0),
-    Planet(3, "Earth", "home planet", 1.0),
-    Planet(4, "Mars", "red planet", 1.25),
-    Planet(5, "Jupiter", "largest planet", 0.42),
-    Planet(6, "Saturn", "ring planet", .45),
-    Planet(7, "Uranus", "coldest planet", .71),
-    Planet(8, "Neptune", "not visible to the naked eye", .67),
-    Planet(9, "Pluto", "unqualified planet", 6.375)
-    ]
-
 planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
 
 @planets_bp.route("", methods=["GET"])  
 def handle_planets():
     results = []    
     for planet in planets:
-        results.append(dict(
-            id=planet.id,
-            name=planet.name,
-            description=planet.description,
-            solar_day=planet.solar_day
-        ))
+        results.append(planet.to_dict())
 
     return jsonify(results)
+
 @planets_bp.route("/<planet_id>", methods=["GET"])
 def handle_planet(planet_id):
     planet = validate_planet(planet_id)
 
-    return {
-        "id": planet.id,
-        "name": planet.name,
-        "description": planet.description,
-        "solar_day": planet.solar_day
-    }
+    return planet.to_dict()
+
+
+def validate_planet(planet_id):
+    try:
+        planet_id = int(planet_id)
+    except:
+        abort(make_response({"message":f"planet {planet_id} invalid"}, 400))
+
+    for planet in planets:
+        if planet.id == planet_id:
+            return planet
+
+    abort(make_response({"message":f"planet {planet_id} not found"}, 404))
