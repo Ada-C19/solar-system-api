f6edf5fc166df49c5a63b02a975461c99bc231a7
Danica <danicas.edu@gmail.com>
2023-04-24 15:12:57 -0700

    Wave 1 & Wave 2 commits

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify, abort, make_response
 
 class Planet:
     def __init__(self, id, name, description, color):
@@ -15,3 +15,43 @@ planets = [
 ]
 
 planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
+
+
+def validate_planet(planet_id):
+    try:
+        planet_id = int(planet_id)
+    except:
+        abort(make_response({"message":f"planet {planet_id} invalid"}, 400))
+    
+    for planet in planets:
+        if planet.id == planet_id:
+            return planet
+        
+    abort(make_response({"message":f"planet {planet_id} not found"}, 404))
+
+
+# Endpoint to get all planets
+@planets_bp.route("", methods=["GET"])
+def handle_planets():
+    planets_response = []
+    for planet in planets:
+        planets_response.append({
+            "id": planet.id,
+            "name": planet.name,
+            "description": planet.description,
+            "color": planet.color
+        })
+    return jsonify(planets_response), 200
+
+# Endpoint to get a planet
+@planets_bp.route("/<planet_id>", methods=["GET"])
+def handle_planet(planet_id):
+    planet = validate_planet(planet_id)
+
+    return {
+        "id": planet.id,
+        "name": planet.name,
+        "description": planet.description,
+        "color": planet.color,
+    }
+
