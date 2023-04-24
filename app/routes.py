47a663d1672f4a986ee270984524078b19aeb8ed
anclark686 <anclark686@gmail.com>
2023-04-24 14:50:17 -0400

    finishing wave 2

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, abort, make_response
 
 class Planet:
     def __init__(self, id, name, description, color):
@@ -6,6 +6,14 @@ class Planet:
         self.name = name
         self.description = description
         self.color = color
+
+    def make_planet_dict(self):
+        return dict(
+            id=self.id,
+            name=self.name,
+            description=self.description,
+            color=self.color
+        )
 
 planets = [
     Planet(1, "Mercury", "hot", "grey"), 
@@ -20,15 +28,29 @@ planets = [
 
 planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
 
+def validate_planet(planet_id):
+    try:
+        planet_id = int(planet_id)
+    except: 
+        abort(make_response({"message": f"Planet with id {planet_id} is invalid"}, 400))
+    
+    for planet in planets:
+        if planet.id == planet_id:
+            return planet
+    
+    return abort(make_response({"message": f"Planet with id {planet_id} was not found"}, 404))
+
+
 @planets_bp.route("", methods = ["GET"])
 def handle_planets():
     planets_response = []
     for planet in planets:
-        planets_response.append(dict(
-            id = planet.id,
-            name = planet.name, 
-            description = planet.description, 
-            color = planet.color
-        ))
+        planets_response.append(planet.make_planet_dict())
     return jsonify(planets_response), 200
 
+
+@planets_bp.route("/<planet_id>", methods = ["GET"])
+def handle_planet(planet_id):
+    planet = validate_planet(planet_id)
+
+    return planet.make_planet_dict()
