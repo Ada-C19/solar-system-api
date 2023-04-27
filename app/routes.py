9c736e0eaba3e87f1a55d72881333d8df1454e20
Charlie <charliethemushi@gmail.com>
2023-04-27 13:50:38 -0700

    creatd route to post new planet in db

--- app/routes.py
+++ app/routes.py
@@ -1,29 +1,39 @@
-from flask import Blueprint, jsonify, abort, make_response
-import json
+from app import db
+from app.models.Planet import Planet
+from flask import Blueprint, jsonify, make_response, request
 
-class Planet():
+# class Planet():
 
-    def __init__(self, id, name, description, place):
-        self.id = id 
-        self.name = name 
-        self.description = description
-        self.place = place
+#     def __init__(self, id, name, description, place):
+#         self.id = id 
+#         self.name = name 
+#         self.description = description
+#         self.place = place
 
-Planets = [
-    Planet(1, "Mercury", "filler", "First"),
-    Planet(2, "Venus", "Filler", "Second"),
-    Planet(3, "Earth", "Filler", "Third"),
-    Planet(4, "Mars", "Filler", "Forth"),
-    Planet(5, "Jupiter", "Filler", "Fifth"),
-    Planet(6, "Saturn", "Filler", "Sixth"),
-    Planet(7, "Uranus", "Filler", "Seventh"),
-    Planet(8, "Neptune", "Filler", "Eighth")
-]
+# Planets = [
+#     Planet(1, "Mercury", "filler", "First"),
+#     Planet(2, "Venus", "Filler", "Second"),
+#     Planet(3, "Earth", "Filler", "Third"),
+#     Planet(4, "Mars", "Filler", "Forth"),
+#     Planet(5, "Jupiter", "Filler", "Fifth"),
+#     Planet(6, "Saturn", "Filler", "Sixth"),
+#     Planet(7, "Uranus", "Filler", "Seventh"),
+#     Planet(8, "Neptune", "Filler", "Eighth")
+# ]
 
 
-planets = Blueprint ("planets", __name__, url_prefix="/planets")
+planets = Blueprint("planets", __name__, url_prefix="/planets")
+@planets.route("", methods=["POST"])
 
-@planets.route("", methods=["GET"])
+def handle_planet():
+    request_body = request.get_json()
+    new_planet = Planet(name=request_body["name"],
+                    description=request_body["description"],
+                    place=request_body["place"])
+    db.session.add(new_planet)
+    db.session.commit()
+
+    return make_response(f"Planet {new_planet.name} succesfully created",201)
 
 #to get all existing planets
 #see a list of planets and their attributes
@@ -32,21 +42,21 @@ planets = Blueprint ("planets", __name__, url_prefix="/planets")
 #     return jsonify(all_planets)
 
 # returns a dictionary
-def return_planets():
-    planet_list = []
+# def return_planets():
+#     planet_list = []
     
-    for planet in Planets:
-        planet_dict = {
-            "id": planet.id,
-            "name": planet.name,
-            "description": planet.description,
-            "place": planet.place
-        }
-        planet_list.append(planet_dict)
+#     for planet in Planets:
+#         planet_dict = {
+#             "id": planet.id,
+#             "name": planet.name,
+#             "description": planet.description,
+#             "place": planet.place
+#         }
+#         planet_list.append(planet_dict)
 
 
-    planets_json = json.dumps(planet_list)
-    return planets_json
+#     planets_json = json.dumps(planet_list)
+#     return planets_json
 
 
 
