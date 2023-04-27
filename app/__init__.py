c72ce6bb61d8d1b7fe0bc73f37477b53aa0170ce
chickenandegg <tei.villanueva@gmail.com>
2023-04-26 18:52:40 -0700

    configure database in app/__init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,20 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+#create instances
+db = SQLAlchemy()
+migrate = Migrate()
 
 
 def create_app(test_config=None):
     app = Flask(__name__)
+
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/solar_system_db_practice'
+
+    db.init_app(app)
+    migrate.init_app(app, db)
 
     from .planets import planet_bp
     app.register_blueprint(planet_bp)
