8b74ec5877c696db32a9ffb43f8f74b39de77f09
anclark686 <anclark686@gmail.com>
2023-04-28 15:02:12 -0400

    adjusting model to avoid error from 'not_null'

--- app/models/planet.py
+++ app/models/planet.py
@@ -2,7 +2,7 @@ from app import db
 
 class Planet(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name = db.Column(db.String)
+    name = db.Column(db.String, nullable=False)
     description = db.Column(db.String)
     color = db.Column(db.String)
 
