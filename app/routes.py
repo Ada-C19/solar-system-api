89ee99e63326bb321213bc7852787a6709f6b5c6
Charlie <charliethemushi@gmail.com>
2023-05-01 13:08:22 -0700

    removed comments

--- app/routes.py
+++ app/routes.py
@@ -2,29 +2,10 @@ from app import db
 from app.models.Planet import Planet
 from flask import Blueprint, jsonify, make_response, request
 
-# class Planet():
-
-#     def __init__(self, id, name, description, place):
-#         self.id = id 
-#         self.name = name 
-#         self.description = description
-#         self.place = place
-
-# Planets = [
-#     Planet(1, "Mercury", "filler", "First"),
-#     Planet(2, "Venus", "Filler", "Second"),
-#     Planet(3, "Earth", "Filler", "Third"),
-#     Planet(4, "Mars", "Filler", "Forth"),
-#     Planet(5, "Jupiter", "Filler", "Fifth"),
-#     Planet(6, "Saturn", "Filler", "Sixth"),
-#     Planet(7, "Uranus", "Filler", "Seventh"),
-#     Planet(8, "Neptune", "Filler", "Eighth")
-# ]
-
 
 planets = Blueprint("planets", __name__, url_prefix="/planets")
-@planets.route("", methods=["POST"])
 
+@planets.route("", methods=["POST"])
 def handle_planet():
     request_body = request.get_json()
     new_planet = Planet(name=request_body["name"],
@@ -35,8 +16,8 @@ def handle_planet():
 
     return make_response(f"Planet {new_planet.name} succesfully created",201)
 
-@planets.route("", methods=["GET"])
 
+@planets.route("", methods=["GET"])
 def return_all_planets():
 
     planets= Planet.query.all()
@@ -52,35 +33,7 @@ def return_all_planets():
     return jsonify(planet_response)
 
 
-
-
-#to get all existing planets
-#see a list of planets and their attributes
-# def request_planets():
-#     all_planets = Planets[id(name,description,place)]
-#     return jsonify(all_planets)
-
-# returns a dictionary
-# def return_planets():
-#     planet_list = []
-    
-#     for planet in Planets:
-#         planet_dict = {
-#             "id": planet.id,
-#             "name": planet.name,
-#             "description": planet.description,
-#             "place": planet.place
-#         }
-#         planet_list.append(planet_dict)
-
-
-#     planets_json = json.dumps(planet_list)
-#     return planets_json
-
-
-
 @planets.route("/<planet_id>", methods=["GET"])
-
 def returns_one_planet_info(planet_id):
     try:
         planet_id = int(planet_id)
@@ -90,16 +43,5 @@ def returns_one_planet_info(planet_id):
         if int(planet_id) == planet.id:
             return vars(planet), 200
     return {"message":f"planet {planet_id} not found"}, 404
-
-
-
-        
-#             #return vars(planet), 200
-#             # return jsonify({
-#             #     "name" : planet.name
-#             #     "id" :  planet.id
-#             #     "description" : planet.description
-#             #     "place" : planet.place
-#             # })
 
 
