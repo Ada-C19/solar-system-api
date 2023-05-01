99fa769e9ac048e26e879143a4616ca989e6b7bb
nadia <reach.nadia.saria@gmail.com>
2023-04-30 17:18:10 -0700

    defines create method for Planet

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,19 @@
-from flask import Blueprint, jsonify, abort, make_response
+from app import db
+from app.models.planet import Planet
+from flask import Blueprint, jsonify, abort, make_response, request
+
+planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
+
+@planets_bp.route("", methods=["POST"])
+def create_planet():
+    request_body = request.get_json()
+    new_planet = Planet(name=request_body["name"],
+                        description=request_body["description"],
+                        solar_day=request_body["solar day"])
+    db.session.add(new_planet)
+    db.session.commit()
+
+    return make_response(f"Planet {new_planet.name} created successfully.", 201)
 
 # Define a Planet class with the attributes id, name, 
 # and description, and one additional attribute
@@ -34,29 +49,28 @@ from flask import Blueprint, jsonify, abort, make_response
         # read one planet
         # return 400 for invalid id
         # return 404 for non existing planet
-planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
 
-@planets_bp.route("", methods=["GET"])  
-def handle_planets():
-    results = [planet.to_dict() for planet in planets]
+# @planets_bp.route("", methods=["GET"])  
+# def handle_planets():
+#     results = [planet.to_dict() for planet in planets]
 
-    return jsonify(results)
+#     return jsonify(results)
 
-@planets_bp.route("/<planet_id>", methods=["GET"])
-def handle_planet(planet_id):
-    planet = validate_planet(planet_id)
+# @planets_bp.route("/<planet_id>", methods=["GET"])
+# def handle_planet(planet_id):
+#     planet = validate_planet(planet_id)
 
-    return planet.to_dict()
+#     return planet.to_dict()
 
 
-def validate_planet(planet_id):
-    try:
-        planet_id = int(planet_id)
-    except:
-        abort(make_response({"message":f"planet {planet_id} invalid"}, 400))
+# def validate_planet(planet_id):
+#     try:
+#         planet_id = int(planet_id)
+#     except:
+#         abort(make_response({"message":f"planet {planet_id} invalid"}, 400))
 
-    for planet in planets:
-        if planet.id == planet_id:
-            return planet
+#     for planet in planets:
+#         if planet.id == planet_id:
+#             return planet
 
-    abort(make_response({"message":f"planet {planet_id} not found"}, 404))
+#     abort(make_response({"message":f"planet {planet_id} not found"}, 404))
