b2c8f11e229adf9952bbf19893614777d2cd08e7
Allie Soliz <solizale@gmail.com>
2023-05-01 01:07:07 -0400

    adds endpoint to delete one planet

--- app/routes.py
+++ app/routes.py
@@ -85,6 +85,15 @@ def update_planet(planet_id):
 
     return make_response(f"Planet #{planet_id} successfully updated")
 
+@planets_bp.route("/<planet_id>", methods=["DELETE"])
+def delete_planet(planet_id):
+    planet = validate_planet(planet_id)
+
+    db.session.delete(planet)
+    db.session.commit()
+
+    return make_response(f"Planet #{planet.id} successfully deleted")
+                  
 
 # class Planet:
 #     """Create class Planet"""
