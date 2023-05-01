3f43d48827ccebd5d10833b529e163133e24dac7
Danica <danicas.edu@gmail.com>
2023-05-01 12:47:18 -0700

    wave 3

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, jsonify, abort, make_response
+from app import db
+from flask import Blueprint, jsonify, abort, make_response, request
 from app.models.planet import Planet
 
 # class Planet:
@@ -17,7 +18,30 @@ from app.models.planet import Planet
 
 planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
 
+@planets_bp.route("", methods=["GET", "POST"])
+def handle_planets():
+    if request.method == "GET":
+        planets = Planet.query.all()
+        planets_response = []
+        for planet in planets:
+            planets_response.append({
+                "id": planet.id,
+                "name": planet.name,
+                "description": planet.description,
+                "color": planet.color
+            })
+        return jsonify(planets_response)
+    elif request.method == "POST":
+        request_body = request.get_json()
+        new_planet = Planet(name=request_body["name"],
+                        description=request_body["description"],
+                        color=request_body["color"])
+    
 
+        db.session.add(new_planet)
+        db.session.commit()
+
+        return make_response(f"Planet {new_planet.name} successfully created.", 201)
 # def validate_planet(planet_id):
 #     try:
 #         planet_id = int(planet_id)
