7f9f35d4c08258d130e28c68668d50d30dde1b78
Danica <danicas.edu@gmail.com>
2023-05-02 11:40:17 -0700

    reads, creates, deletes one endpoint (wave 4)

--- app/routes.py
+++ app/routes.py
@@ -17,6 +17,58 @@ from app.models.planet import Planet
 # ]
 
 planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
+#VALIDATE PLANET HELPER FN
+def validate_planet(planet_id):
+    try:
+        planet_id = int(planet_id)
+    except:
+        abort(make_response({"message":f"planet {planet_id} invalid"}, 400))
+    
+    planet = Planet.query.get(planet_id)
+
+    if not planet:    
+        abort(make_response({"message":f"planet {planet_id} not found"}, 404))
+    
+    return planet
+
+# GET ONE ENDPOINT
+@planets_bp.route("/<planet_id>", methods=["GET"])
+def handle_planet(planet_id):
+    # planet = Planet.query.get(planet_id)
+    planet = validate_planet(planet_id)
+
+    return {
+        "id": planet.id,
+        "name": planet.name,
+        "description": planet.description,
+        "color": planet.color
+    }, 200
+
+# UPDATE ONE ENDPOINT
+@planets_bp.route("/<planet_id>", methods=["PUT"])
+def update_planet(planet_id):
+    planet = validate_planet(planet_id)
+
+    request_body = request.get_json()
+
+    planet.name = request_body["name"]
+    planet.description = request_body["description"]
+    planet.color = request_body["color"]
+
+    db.session.commit()
+
+    return make_response(f"Planet {planet.id} successfully updated!"), 200
+
+# DELETE ONE ENDPOINT
+@planets_bp.route("/<planet_id>", methods=["DELETE"])
+def delete_planet(planet_id):
+    planet = validate_planet(planet_id)
+
+    db.session.delete(planet)
+    db.session.commit()
+
+    return make_response(f"Planet {planet.id} successfully deleted!"), 200
+
 
 @planets_bp.route("", methods=["GET", "POST"])
 def handle_planets():
@@ -37,23 +89,23 @@ def handle_planets():
                         description=request_body["description"],
                         color=request_body["color"])
     
-
         db.session.add(new_planet)
         db.session.commit()
 
         return make_response(f"Planet {new_planet.name} successfully created.", 201)
+    
 # def validate_planet(planet_id):
 #     try:
 #         planet_id = int(planet_id)
 #     except:
 #         abort(make_response({"message":f"planet {planet_id} invalid"}, 400))
     
-#     for planet in planets:
-#         if planet.id == planet_id:
-#             return planet
-        
-#     abort(make_response({"message":f"planet {planet_id} not found"}, 404))
+#     planet = Planet.query.get(planet_id)
 
+#     if not planet:    
+#         abort(make_response({"message":f"planet {planet_id} not found"}, 404))
+    
+#     return planet
 
 # # Endpoint to get all planets
 # @planets_bp.route("", methods=["GET"])
