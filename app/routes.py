38248ffd1d054ee6df8c0abc4311cff04bf60816
Arianna Walden <nedlawa@gmail.com>
2023-05-02 11:09:27 -0700

    add read single planet, update, and delete

--- app/routes.py
+++ app/routes.py
@@ -12,6 +12,7 @@ from flask import Blueprint, jsonify, make_response, request, abort
 
 bp = Blueprint("planets", __name__, url_prefix="/planets")
 
+
 def validate_planet(id):
     try:
         id = int(id)
@@ -22,18 +23,19 @@ def validate_planet(id):
 
     if not planet:
         abort(make_response({'message': f'Planet {id} not found.'}, 404))
-        
+
     return planet
 
-@planets_bp.route("/<id>", methods=["GET"])
-def handle_planet(id):
+
+@bp.route("/<id>", methods=["GET"])
+def read_one_planet(id):
     planet = validate_planet(id)
 
-    return planet.make_planet_dict()
+    return planet.make_dict()
 
 
 @bp.route("", methods=["GET"])
-def handle_planets():
+def read_all_planets():
     planets = Planet.query.all()
 
     planets_response = [Planet.make_dict(planet) for planet in planets]
@@ -46,10 +48,32 @@ def create_planet():
     request_body = request.get_json()
     new_planet = Planet(name=request_body["name"],
                         description=request_body["description"],
-                        association=request_body["association"],
-                        )
+                        association=request_body["association"])
 
     db.session.add(new_planet)
     db.session.commit()
 
     return make_response(f'Planet {new_planet} successfully created.', 201)
+
+@bp.route("/<id>", methods=["PUT"])
+def update_planet(id):
+    planet = validate_planet(id)
+    request_body = request.get_json()
+
+    planet.name = request_body["name"]
+    planet.description = request_body["description"]
+    planet.association = request_body["association"]
+
+    db.session.commit()
+
+    return make_response(jsonify(f"Planet #{planet.id} successfully updated"))
+
+
+@bp.route("/<id>", methods=["DELETE"])
+def delete_planet(id):
+    planet = validate_planet(id)
+
+    db.session.delete(planet)
+    db.session.commit()
+
+    return make_response(jsonify(f"Planet #{planet.id} successfully deleted"))
