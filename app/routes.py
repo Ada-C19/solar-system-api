568fdb611d38fce8bce2e080d667a317df80a472
Barbara <barbaraelisabethb@gmail.com>
2023-05-02 15:01:01 -0400

    Completed Wave_04

--- app/routes.py
+++ app/routes.py
@@ -25,8 +25,47 @@ def create_planet():
 
     return make_response(f"Planet {new_planet.name} successfully created", 201) 
 
+@bp.route("/<planet_id>", methods=["GET"])
+def read_one_planet(planet_id):
+    planet = validate_planet(planet_id)
 
+    return jsonify(planet.make_planet_dict()), 200
 
+@bp.route("/<planet_id>", methods=["PUT"])
+def update_planet(planet_id):
+    planet = validate_planet(planet_id)
+    request_body = request.get_json()
+
+    planet.name=request_body["name"],
+    planet.description=request_body["description"],
+    planet.position=request_body["position"]
+    
+    db.session.commit()
+
+    return make_response(f"Planet {planet.name} successfully updated", 200)
+
+@bp.route("/<planet_id>", methods=["DELETE"])
+def delete_planet(planet_id):
+  planet = validate_planet(planet_id)
+
+  db.session.delete(planet)
+  db.session.commit()
+
+  return make_response(f"Planet {planet.name} successfully deleted", 200)
+
+# helper function
+def validate_planet(planet_id):
+    try:
+        planet_id = int(planet_id)
+    except:
+        abort(make_response({"message":f"Planet {planet_id} invalid"}, 400))
+
+    planet = Planet.query.get(planet_id)
+    
+    if not planet:
+        abort(make_response({"message":f"Planet {planet_id} not found"}, 404))
+        
+    return planet
 
 # class Planet:
 #     def __init__(self, id, name, description, position):
@@ -36,7 +75,7 @@ def create_planet():
 #         self.position = position
 
 # planets = [
-#     Planet(1, "Mercury", "Closest to the sun and smallest", "#1"),
+#     Planet(1, ),
 #     Planet(2, "Venus", "The hottest planet of the Solar System", "#2"), 
 #     Planet(3, "Earth", "Seventy percent of its surface is cover with water", "#3"), 
 #     Planet(4, "Mars", "Known as Red Planet because of iron oxide on its surface", "#4"),
@@ -45,40 +84,3 @@ def create_planet():
 #     Planet(7, "Uranus", "It is the coldest planet of the Solar System with temperatures at around -224 degrees Celsius", "#7"),
 #     Planet(8, "Neptune", "Has the fasted wind speeds of any planet, reaching speeds of 2.160 km / 1.314 mi per hour", "#8")
 #     ]
-
-# @bp.route("", methods=["GET"])
-# def handle_planets():
-#     planets_response = []
-#     for planet in planets:
-#         planets_response.append(
-#             {
-#             "id": planet.id,
-#             "name": planet.name,
-#             "description": planet.description,
-#             "position": planet.position
-#             }
-#         )
-#     return jsonify(planets_response)
-
-# def validate_planet(planet_id):
-#     try:
-#         planet_id = int(planet_id)
-#     except:
-#         abort(make_response({"message":f"Planet {planet_id} invalid"}, 400))
-
-#     for planet in planets:
-#         if planet.id == planet_id:
-#             return planet
-        
-#     abort(make_response({"message":f"Planet {planet_id} not found"}, 404))
-
-# @bp.route("/<planet_id>", methods=["GET"])
-# def handle_planet(planet_id):
-#     planet = validate_planet(planet_id)
-    
-#     return {
-#             "id": planet.id,
-#             "name": planet.name,
-#             "description": planet.description,
-#             "position": planet.position
-#             }
