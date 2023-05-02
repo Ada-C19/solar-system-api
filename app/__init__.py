a9bea5be98a5cad983e49511f6210caf4db7c2bb
Allie Soliz <solizale@gmail.com>
2023-05-02 18:24:49 -0400

    imports book module

--- app/__init__.py
+++ app/__init__.py
@@ -8,15 +8,20 @@ db = SQLAlchemy()
 migrate = Migrate()
 load_dotenv()
 
+
 def create_app(test_config=None):
     app = Flask(__name__)
-    
+
     if not test_config:
         app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-        app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("SQLALCHEMY_DATABASE_URI")
+        app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+            "SQLALCHEMY_DATABASE_URI")
     else:
-        
-        app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("SQLALCHEMY_TEST_DATABASE_URI")
+
+        app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+            "SQLALCHEMY_TEST_DATABASE_URI")
+
+    from app.models.planet import Planet
 
     db.init_app(app)
     migrate.init_app(app, db)
