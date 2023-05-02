99fffb11cca607aa584593f295dba9e0e71f2d02
chickenandegg <tei.villanueva@gmail.com>
2023-05-02 11:25:23 -0700

    created .env file and imported loadenv

--- app/__init__.py
+++ app/__init__.py
@@ -2,16 +2,35 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
+from dotenv import load_dotenv
+import os
+
 #create instances
 db = SQLAlchemy()
 migrate = Migrate()
 
+load_dotenv()
 
-def create_app(test_config=None):
+
+def create_app(testing=None):
     app = Flask(__name__)
 
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@127.0.0.1:5432/solar_system_development'
+
+    if testing is None:
+
+        app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+            "SQLALCHEMY_DATABASE_URI")
+        
+    else:
+        app.config["SQLALCHEMY_TEST_DATABASE_TEST"] = os.environ.get(
+            "SQLALCHEMY_TEST_DATABASE_TEST")
+        
+    
+    # app.config["DEBUG"] = True
+
+
+    
 
 
     from.model.planets_model import Planet
