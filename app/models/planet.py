91d8c5f0cc12dfd602d2c7e7cf9ccd5198024cf5
Jessica <Yiskah.Scientia@gmail.com>
2023-05-02 18:24:47 -0600

    Wave 4 - broken

--- app/models/planet.py
+++ app/models/planet.py
@@ -1,20 +1,11 @@
 from app import db
 
 class Planet(db.Model):
-        __tablename__ = 'planets'
 
         id = db.Column(db.Integer, primary_key=True, autoincrement=True)
         name = db.Column(db.String, nullable=False)
         description = db.Column(db.String, nullable=False)
         mass = db.Column(db.Numeric(precision=8, scale=3), nullable=False)
-        #     moons = db.Column(db.Integer, nullable=False)
-        #     distance_from_sun = db.Column(db.Numeric(precision=8, scale=3), nullable=False)
-        #     surface_area = db.Column(db.Numeric(precision=8, scale=3), nullable=False)
-        #     namesake = db.Column(db.String, nullable=False)
-        #     visited_by_humans = db.Column(db.Boolean, nullable=False, default=False)
-
-        def __repr__(self):
-                return f'<Planet {self.name}>'
 
         def to_dict(self):
                 return {
@@ -22,9 +13,4 @@ class Planet(db.Model):
                 'name': self.name,
                 'description': self.description,
                 'mass': self.mass,
-                #     'moons': self.moons,
-                #     'distance_from_sun': self.distance_from_sun,
-                #     'surface_area': self.surface_area,
-                #     'namesake': self.namesake,
-                #     'visited_by_humans': self.visited_by_humans
                 }
--- app/routes.py
+++ app/routes.py
@@ -4,29 +4,30 @@ from app import db
 
 planets_bp = Blueprint('planets', __name__, url_prefix='/planets')
 
-@planets_bp.route("", methods=['POST'])
-def create_planet():
-    request_body = request.get_json()
-    new_planet = Planet(
-        name = request_body["name"],
-        description = request_body["description"],
-        mass = request_body["mass"],
-        # moons = request_body["moons"],
-        # distance_from_sun = request_body["distance_from_sun"],
-        # surface_area = request_body["surface_area"],
-        # namesake = request_body["namesake"],
-        # visited_by_humans = request_body['visited_by_humans'],
-    )
+# @planets_bp.route("", methods=['POST'])
+# def create_planet():
+#     request_body = request.get_json()
+#     new_planet = Planet(
+#         name = request_body["name"],
+#         description = request_body["description"],
+#         mass = request_body["mass"],
+#         # moons = request_body["moons"],
+#         # distance_from_sun = request_body["distance_from_sun"],
+#         # surface_area = request_body["surface_area"],
+#         # namesake = request_body["namesake"],
+#         # visited_by_humans = request_body['visited_by_humans'],
+#     )
 
-    db.session.add(new_planet)
-    db.session.commit()
+#     db.session.add(new_planet)
+#     db.session.commit()
 
-    message = f"New planet {new_planet.name} successfully created!"
-    return make_response(message, 201)
+#     message = f"New planet {new_planet.name} successfully created, biatch!"
+
+#     return make_response(message, 201)
 
 @planets_bp.route("", methods=["GET"])
 def get_planets():
-    planets = planets.all()
+    planets = Planet.query.all()
     request_body = []
     for planet in planets:
         request_body.append(
@@ -34,76 +35,59 @@ def get_planets():
                 id=planet.id,
                 name=planet.name,
                 description=planet.description,
-                mass=planet.mass,
-                # moons=planet.moons,
-                # distance_from_sun=planet.distance_from_sun,
-                # surface_area=planet.surface_area,
-                # namesake=planet.namesake,
-                # visited_by_humans=planet.visited_by_humans,
+                mass=planet.mass
             )
         )
     return jsonify(request_body), 200
 
-
-
-
-
-
-
-
-# from flask import Blueprint, jsonify, abort, make_response
-
-# class Planet:
-#     def __init__(self, id, name, description, mass):
-#         self.id = id
-#         self.name = name
-#         self.description = description
-#         self.mass = mass
-
-#     def to_dict(self):
-#         return dict(
-#             id = self.id, 
-#             name = self.name, 
-#             description = self.description, 
-#             mass = self.mass
-#         )
-        
-# planet_list = [
-#     Planet(1, "Mercury", "Small, rocky planet closest to the Sun", 0.330),
-#     Planet(2, "Venus", "Hottest planet in the solar system with a thick atmosphere", 4.87),
-#     Planet(3, "Earth", "Home to a diverse range of life forms, including humans", 5.97),
-#     Planet(4, "Mars", "Red planet with a thin atmosphere and polar ice caps", 0.642),
-#     Planet(5, "Jupiter", "Largest planet in the solar system with a thick atmosphere and many moons", 1898),
-#     Planet(6, "Saturn", "Known for its prominent rings made of ice and dust", 568),
-#     Planet(7, "Uranus", "Blue-green planet with a tilted axis of rotation", 86.8),
-#     Planet(8, "Neptune", "Blue planet with a windy atmosphere and many storms", 102),
-# ]
-
-# planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
-
-# @planets_bp.route("", methods=["GET"])
-# def get_planets():
-#     planets_response = []
-#     for planet in planet_list:
-#         planets_response.append(planet.to_dict())
-
-#     return jsonify(planets_response), 200
-
-# @planets_bp.route("/<id>", methods=["GET"])
-# def get_planet(id):
-#     planet = validate_planet(id)
-
-#     return planet.to_dict()
-
-# def validate_planet(id):
+# def validate_planet(planet_id):
 #     try:
-#         id = int(id)
-    
+#         planet_id = int(planet_id)
 #     except:
-#         abort(make_response({"message": f"Planet {id} is invalid!"}, 400))
+#         abort(make_response({"message": f"Planet {planet_id} is invalid, sucker!"}, 400))
     
-#     for planet in planet_list:
-#         if planet.id == id:
-#             return planet
+#     planet = Planet.query.get(planet_id)
 
-#     abort(make_response({"message": f"Planet {id} is not found!"}, 404))
+#     if not planet:
+#         abort(make_response({"message": f"Planet {planet_id} was not found, sucker!"}, 404))
+
+#     return planet
+
+# @planets_bp.route("/<planet_id>", methods=['GET'])
+# def read_one_planet(planet_id):
+#     planet = validate_planet(planet_id)
+#     return {         
+#         "id" : planet.id,   
+#         "name": planet.name,
+#         "description": planet.description,
+#         "mass": planet.mass
+#         }
+
+# @planets_bp.route("/<planet_id>", methods=['PUT'])
+# def update_planet(planet_id):
+#         planet = validate_planet(planet_id)
+#         request_body = request.get_json()
+            
+#         planet.name = request_body["name"]
+#         planet.description = request_body["description"]
+#         planet.mass = request_body["mass"]
+
+#         db.session.commit()
+
+#         message = f"Planet {planet} succesfully updated, biatch!"
+
+#         return make_response({"message": message}, 200)
+
+# @planets_bp.route("/<planet_id>", methods=['DELETE'])
+# def delete_planet(planet_id):
+#         planet = validate_planet(planet_id)
+
+#         db.session.delete(planet)
+#         db.session.commit()
+
+#         message = f"Planet {planet} succesfully deleted, biatch!"
+
+#         return make_response({"message": message}, 200)
+
+
+
