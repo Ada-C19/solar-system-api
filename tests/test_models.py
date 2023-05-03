cfaac80c655ce3dc018216d90d1bdc81dc074091
Gabby <gabrielleyoung10@gmail.com>
2023-05-02 21:48:28 -0500

    Add tests for routes and model

--- tests/test_models.py
+++ tests/test_models.py
@@ -71,3 +71,63 @@ def test_to_dict_missing_distance():
     assert result["name"] == "Mercury"
     assert result["description"] == "The smallest planet; pitted and streaky, brownish-gray"
     assert result["distance_from_the_sun"] is None
+
+
+def test_from_dict_returns_planet():
+    planet_data = {
+        "name": "Saturn",
+        "description": "A yellowish gas giant with rings made of ice and rock",
+        "distance_from_the_sun": 9.54
+    }
+
+    new_planet = Planet.from_dict(planet_data)
+
+    assert new_planet.name == "Saturn"
+    assert new_planet.description == "A yellowish gas giant with rings made of ice and rock"
+    assert new_planet.distance_from_the_sun == 9.54
+
+
+def test_from_dict_with_no_name():
+    planet_data = {
+        "description": "A yellowish gas giant with rings made of ice and rock",
+        "distance_from_the_sun": 9.54
+    }
+
+    with pytest.raises(KeyError, match="name"):
+        new_planet = Planet.from_dict(planet_data)
+
+
+def test_from_dict_with_no_description():
+    planet_data = {
+        "name": "Saturn",
+        "distance_from_the_sun": 9.54
+    }
+
+    with pytest.raises(KeyError, match="description"):
+        new_planet = Planet.from_dict(planet_data)
+
+
+def test_from_dict_with_no_distance():
+    planet_data = {
+        "name": "Saturn",
+        "description": "A yellowish gas giant with rings made of ice and rock",
+    }
+
+    with pytest.raises(KeyError, match="distance_from_the_sun"):
+        new_planet = Planet.from_dict(planet_data)
+
+
+def test_from_dict_with_extra_keys():
+    planet_data = {
+        "surface_area": 16.49,
+        "name": "Saturn",
+        "description": "A yellowish gas giant with rings made of ice and rock",
+        "distance_from_the_sun": 9.54,
+        "age": 4.503
+    }
+
+    new_planet = Planet.from_dict(planet_data)
+
+    assert new_planet.name == "Saturn"
+    assert new_planet.description == "A yellowish gas giant with rings made of ice and rock"
+    assert new_planet.distance_from_the_sun == 9.54
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,3 +1,4 @@
+import pytest
 def test_get_all_planets_with_no_records(client):
     response = client.get("/planets")
     response_body = response.get_json()
@@ -81,12 +82,143 @@ def test_get_one_planet_invalid_id(client, two_saved_planets):
 
 
 def test_create_one_planet(client):
-    response = client.post("/planets", json={
+    test_data = {
         "name": "Earth",
         "description": "A beautiful blue-green planet with life-supporting atmosphere",
         "distance_from_the_sun": 1.00
-    })
+    }
+    response = client.post("/planets", json=test_data)
     response_body = response.get_data(as_text=True)
 
     assert response.status_code == 201
     assert response_body == "Planet Earth successfully created"
+
+
+def test_create_one_planet_no_name(client):
+    test_data = {
+        "description": "A beautiful blue-green planet with life-supporting atmosphere",
+        "distance_from_the_sun": 1.00
+    }
+
+    with pytest.raises(KeyError, match="name"):
+        response = client.post("/planets", json=test_data)
+
+
+def test_create_one_planet_no_description(client):
+    test_data = {
+        "name": "Earth",
+        "distance_from_the_sun": 1.00
+    }
+
+    with pytest.raises(KeyError, match="description"):
+        response = client.post("/planets", json=test_data)
+
+
+def test_create_one_planet_no_distance(client):
+    test_data = {
+        "name": "Earth",
+        "description": "A beautiful blue-green planet with life-supporting atmosphere",
+    }
+
+    with pytest.raises(KeyError, match="distance_from_the_sun"):
+        response = client.post("/planets", json=test_data)
+
+
+def test_create_one_planet_with_extra_keys(client, two_saved_planets):
+    test_data = {
+        "surface_area": 196.9,
+        "name": "Earth",
+        "description": "A beautiful blue-green planet with life-supporting atmosphere",
+        "distance_from_the_sun": 1.00,
+        "age": 4.543,
+    }
+
+    response = client.post("/planets", json=test_data)
+    response_body = response.get_data(as_text=True)
+
+    assert response.status_code == 201
+    assert response_body == "Planet Earth successfully created"
+
+
+def test_update_planet(client, two_saved_planets):
+    test_data = {
+        "name": "Uranus",
+        "description": "A blue-green gas giant which also has a ring system",
+        "distance_from_the_sun": 19.19
+    }
+
+    response = client.put("/planets/1", json=test_data)
+    response_body = response.get_data(as_text=True)
+
+    assert response.status_code == 200
+    assert response_body == "Planet #1 successfully updated"
+
+
+def test_update_planet_with_extra_keys(client, two_saved_planets):
+    test_data = {
+        "surface_area": 3.121,
+        "name": "Uranus",
+        "description": "A blue-green gas giant which also has a ring system",
+        "distance_from_the_sun": 19.19,
+        "age": 4.503
+    }
+
+    response = client.put("/planets/1", json=test_data)
+    response_body = response.get_data(as_text=True)
+
+    assert response.status_code == 200
+    assert response_body == "Planet #1 successfully updated"
+
+
+def test_update_planet_missing_record(client, two_saved_planets):
+    test_data = {
+        "name": "Uranus",
+        "description": "A blue-green gas giant which also has a ring system",
+        "distance_from_the_sun": 19.19
+    }
+
+    response = client.put("/planets/300", json=test_data)
+    response_body = response.get_json()
+
+    assert response.status_code == 404
+    assert response_body == {"message": "Planet with id 300 was not found"}
+
+
+def test_update_planet_invalid_id(client, two_saved_planets):
+    test_data = {
+        "name": "Uranus",
+        "description": "A blue-green gas giant which also has a ring system",
+        "distance_from_the_sun": 19.19
+    }
+
+    response = client.put("/planets/nearest", json=test_data)
+    response_body = response.get_json()
+
+    assert response.status_code == 400
+    assert response_body == {"message": "Planet with id nearest is invalid"}
+
+
+def test_delete_one_planet(client, two_saved_planets):
+    response = client.delete("planets/1")
+    response_body = response.get_data(as_text=True)
+
+    assert response.status_code == 200
+    assert response_body == "Planet #1 successfully deleted"
+
+
+def test_delete_missing_planet(client, two_saved_planets):
+    response = client.delete("planets/300")
+    response_body = response.get_json()
+
+    assert response.status_code == 404
+    assert response_body == {"message": "Planet with id 300 was not found"}
+
+
+def test_delete_planet_invalid_id(client, two_saved_planets):
+    response = client.delete("planets/nearest")
+    response_body = response.get_json()
+
+    assert response.status_code == 400
+    assert response_body == {"message": "Planet with id nearest is invalid"}
+
+
