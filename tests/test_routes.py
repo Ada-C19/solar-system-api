b1eddd4582cd2b7fc33ee0df83e233a3d5a4778d
Allie Soliz <solizale@gmail.com>
2023-05-03 12:19:59 -0400

    adds GET /planet/<planet_id> TEST

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -6,3 +6,18 @@ def test_get_all_planets_with_no_records(client):
     # Assert
     assert response.status_code == 200
     assert response_body == []
+
+
+def test_get_one_planet(client, two_saved_planets):
+    # Act
+    response = client.get("/planets/1")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 200
+    assert response_body == {
+        "id": 1,
+        "name": "Mercury Planet",
+        "description": "Smallest planet in the solar system. Its year is 88 days long. Closest to the Sun",
+        "type": "Rocky/Terrestrial"
+    }
