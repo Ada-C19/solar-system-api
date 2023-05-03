2eef14c25b28143b58fa85baecc6b233af758446
Gabby <gabrielleyoung10@gmail.com>
2023-05-02 19:03:21 -0500

    Add query param to read_all_planets()

--- app/routes.py
+++ app/routes.py
@@ -45,13 +45,17 @@ def validate_planet(planet_id):
     return planet
 
 # route functions
-
 @planets_bp.route("", methods=["GET"])
 def read_all_planets():
-    planets = Planet.query.all()
-    planets_response = []
-    for planet in planets:
-        planets_response.append(planet.to_dict())
+    name_query = request.args.get("name")
+  
+    if name_query:
+        planets = Planet.query.filter_by(name=name_query)
+    else:
+        planets = Planet.query.all()
+
+    planets_response = [planet.to_dict() for planet in planets]
+
     return jsonify(planets_response)
 
 @planets_bp.route("", methods=["POST"])
