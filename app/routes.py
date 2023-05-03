27936ea3256ca1acbbcfdc26eb2d345f783981e0
halimAremu <halimat3000@gmail.com>
2023-05-03 13:30:19 -0500

    wave 6 complete

--- app/routes.py
+++ app/routes.py
@@ -42,8 +42,12 @@ def create_planet():
 @planets_bp.route("", methods=['GET'])
 
 def read_all_planets():
+    name_query = request.args.get("name")
+    if name_query:
+        planets = Planet.query.filter_by(name=name_query)
+    else:
+        planets = Planet.query.all()
     planet_records= []
-    planets=Planet.query.all()
     for planet in planets:
         planet_records.append(
             planet.to_dict())
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -24,3 +24,27 @@ def test_get_one_planet_with_no_data(client):
     
     assert response.status_code == 404
     assert response_body == {"message":"planet 1 not found"}
+    
+def test_get_planets_with_valid_test_data(client, saved_planets):
+    response = client.get("/planets")
+    response_body = response.get_json()
+    
+    assert response.status_code == 200
+    assert len(response_body) == 2
+    assert response_body[0] == {"id":1, "name":"Mercury",
+                                "description":"rocky planet", "form":"solid" }       
+
+def test_create_one_planet(client):
+    PLANET_DICT = {"name": "Saturn",
+                   "description":"made of hydrogen",
+                   "form":"gas"}
+    response = client.post("/planets", json=PLANET_DICT)
+    response_body = response.get_json()
+    
+    actual_planet = Planet.query.get(1)
+    
+    assert response.status_code == 201
+    assert response_body == "Planet Saturn was created successfully"
+    assert actual_planet.name == "Saturn"
+    assert actual_planet.description == "made of hydrogen"
+    assert actual_planet.form =="gas"
