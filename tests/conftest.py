0f72de70d4eadb050c1f5f90ea3f4e599271d246
louloera <louloera@gmail.com>
2023-05-03 14:01:13 -0700

    Created test case to return one planet and populated test database with two planets

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,7 @@ import pytest
 from app import create_app
 from app import db
 from flask.signals import request_finished
+from app.models.Planet import Planet
 
 
 @pytest.fixture
@@ -23,3 +24,12 @@ def app():
 @pytest.fixture
 def client(app):
     return app.test_client()
+
+@pytest.fixture
+def two_planets(app):
+    #Arrange
+    venus= Planet (name="Venus",description ="Gassy love goddess", place= 2)
+    earth = Planet(name="Earth",description ="le home", place= 3)
+    
+    db.session.add_all([venus,earth])
+    db.session.commit()
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -6,3 +6,13 @@ def test_get_all_planets_with_no_records(client):
     # Assert
     assert response.status_code == 200
     assert response_body == []
+
+def test_get_all_planets_with_two_records(client, two_planets):
+    # Act
+    response = client.get("/planets/1")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 200
+    assert response_body == {"id": 1,"name":"Venus","description":"Gassy love goddess","place":2}
+#{"id" : 2, "name":"Earth","description" : "le home", "place": 3}]         
