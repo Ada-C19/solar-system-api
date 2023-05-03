1e86bed9e41bac208168bc1b578d3a0c0c676b68
Allie Soliz <solizale@gmail.com>
2023-05-03 12:17:58 -0400

    creates fixture that saves two planets to the database

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,7 @@ import pytest
 from app import create_app
 from app import db
 from flask.signals import request_finished
+from app.models.planet import Planet
 
 
 @pytest.fixture
@@ -23,3 +24,20 @@ def app():
 @pytest.fixture
 def client(app):
     return app.test_client()
+
+
+@pytest.fixture
+def two_saved_planets(app):
+    # Arrange
+    mercury_planet = Planet(name="Mercury Planet",
+                            description="Smallest planet in the solar system. Its year is 88 days long. Closest to the Sun",
+                            type="Rocky/Terrestrial")
+    venus_planet = Planet(name="Venus Planet",
+                          description="Hottest planet in the solar system. Has a thick, toxic atmosphere. Second planet from the Sun",
+                          type="Rocky/Terrestrial")
+
+    db.session.add_all([mercury_planet, venus_planet])
+    # Alternatively, we could do
+    # db.session.add(mercury_planet)
+    # db.session.add(venus_planet)
+    db.session.commit()
