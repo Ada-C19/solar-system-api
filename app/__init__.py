5a129a6bdf3b3882ef9811788906c0bd28191ff6
Jackie <jackiemay08@gmail.com>
2023-05-04 13:20:41 -0400

    Wave 6

--- app/__init__.py
+++ app/__init__.py
@@ -13,10 +13,6 @@ def create_app(test_config=None):
 
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-    # if not test_config:
-    #     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
-    #     app.config['SQLALCHEMY_TEST_DATABASE_URI'] = os.environ.get('SQLALCHEMY_TEST_DATABASE_URI')
-
     if not test_config:
         app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
             "SQLALCHEMY_DATABASE_URI")
