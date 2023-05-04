a2f08270b866e19549c1fc784a8af30516457c22
Amber Shay <mashaad21@gmail.com>
2023-05-03 22:40:36 -0600

    updated test suite for more coverage

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -17,7 +17,7 @@ def test_get_one_planet(client, two_saved_planets):
     assert response.status_code == 200
     assert response_body == {
         "id": 1,
-        "name": "Mercury Planet",
+        "name": "Mercury",
         "description": "Smallest planet in the solar system. Its year is 88 days long. Closest to the Sun",
         "type": "Rocky/Terrestrial"
     }
@@ -36,3 +36,73 @@ def test_create_one_planet(client):
     # Assert
     assert response.status_code == 201
     assert response_body == "Planet New Planet successfully created"
+
+
+def test_get_all_planets_with_two_records(client, two_saved_planets):
+    # Act
+    response = client.get("/planets")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 200
+    assert len(response_body) == 2
+    assert response_body[0] == {
+        "id": 1,
+        "name": "Mercury",
+        "description": "Smallest planet in the solar system. Its year is 88 days long. Closest to the Sun.",
+        "type": "Rocky/Terrestrial"
+    }
+    assert response_body[1] == {
+        "id": 3,
+        "name": "Venus",
+        "description": "Hottest planet in the solar system. Has a thick, toxic atmosphere. Second planet from the Sun.",
+        "type": "Rocky/Terrestrial"
+    }
+
+# When we have records and a `name` query in the request arguments, `get_all_planets` returns a list containing only the planet(s) which match the query
+def test_get_all_planets_with_name_query_matching_none(client, two_saved_books):
+    # Act
+    data = {'name': 'Mercury'}
+    response = client.get("/planets", query_string = data)
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 200
+    assert response_body == []
+
+# When we have records and a `name` query in the request arguments, `get_all_planets` returns a list containing only the planets which match the query
+def test_get_all_planets_with_name_query_matching_one(client, two_saved_books):
+    # Act
+    data = {'name': 'Mercury'}
+    response = client.get("/planets", query_string = data)
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 200
+    assert len(response_body) == 1
+    assert response_body[0] == {
+        "id": 1,
+        "name": "Mercury",
+        "description": "Smallest planet in the solar system. Its year is 88 days long. Closest to the Sun.",
+        "type": "Rocky/Terrestrial"
+    }
+
+# When we call `read_one_planet` with a numeric ID that doesn't have a record, we get the expected error message
+def test_get_one_planet_id_not_found(client, two_saved_books):
+    # Act
+    response = client.get("/planets/2")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 404
+    assert response_body == {"message":"Planet 2 not found"}
+
+# When we call `read_one_planet` with a non-numeric ID, we get the expected error message
+def test_get_one_planet_id_invalid(client, two_saved_books):
+    # Act
+    response = client.get("/planets/Pluto")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 400
+    assert response_body == {"message":"Planet Pluto invalid"}
