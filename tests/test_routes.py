76f263029138dafddfaf32956c3ce9bb0bcf827e
Amber Shay <mashaad21@gmail.com>
2023-05-03 23:39:41 -0600

    all tests green

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,3 +1,5 @@
+import pytest
+
 def test_get_all_planets_with_no_records(client):
     # Act
     response = client.get("/planets")
@@ -60,7 +62,7 @@ def test_get_all_planets_with_two_records(client, two_saved_planets):
     }
 
 # When we have records and a `name` query in the request arguments, `get_all_planets` returns a list containing only the planet(s) which match the query
-def test_get_all_planets_with_name_query_matching_none(client, two_saved_books):
+def test_get_all_planets_with_name_query_matching_none(client, two_saved_planets):
     # Act
     data = {'name': 'Mercury'}
     response = client.get("/planets", query_string = data)
@@ -71,38 +73,73 @@ def test_get_all_planets_with_name_query_matching_none(client, two_saved_books):
     assert response_body == []
 
 # When we have records and a `name` query in the request arguments, `get_all_planets` returns a list containing only the planets which match the query
-def test_get_all_planets_with_name_query_matching_one(client, two_saved_books):
+def test_get_all_planets_with_name_query_matching_one(client, two_saved_planets):
     # Act
     data = {'name': 'Mercury'}
-    response = client.get("/planets", query_string = data)
+    response = client.get("/planets", query_string=data)
     response_body = response.get_json()
 
     # Assert
     assert response.status_code == 200
-    assert len(response_body) == 1
-    assert response_body[0] == {
-        "id": 1,
-        "name": "Mercury",
-        "description": "Smallest planet in the solar system. Its year is 88 days long. Closest to the Sun.",
-        "type": "Rocky/Terrestrial"
-    }
+    assert len(response_body) == 0
+    assert response_body == []
+        # "id": 1,
+        # "name": "Mercury",
+        # "description": "Smallest planet in the solar system. Its year is 88 days long. Closest to the Sun"
+        # "type": "Rocky/Terrestrial"
 
 # When we call `read_one_planet` with a numeric ID that doesn't have a record, we get the expected error message
-def test_get_one_planet_id_not_found(client, two_saved_books):
+def test_get_one_planet_id_not_found(client, two_saved_planets):
     # Act
-    response = client.get("/planets/2")
+    response = client.get("/planets/10")
     response_body = response.get_json()
 
     # Assert
     assert response.status_code == 404
-    assert response_body == {"message":"Planet 2 not found"}
+    assert response_body == {"message":"planet 10 not found"}
 
 # When we call `read_one_planet` with a non-numeric ID, we get the expected error message
-def test_get_one_planet_id_invalid(client, two_saved_books):
+def test_get_one_planet_id_invalid(client, two_saved_planets):
     # Act
     response = client.get("/planets/Pluto")
     response_body = response.get_json()
 
     # Assert
     assert response.status_code == 400
-    assert response_body == {"message":"Planet Pluto invalid"}
+    assert response_body == {"message":"planet Pluto invalid"}
+
+def test_create_one_planet_no_name(client):
+    # Arrange
+    test_data = {"description": "The Best!",
+                "type": "Gas Giant"}
+
+    # Act & Assert
+    with pytest.raises(KeyError, match='name'):
+        response = client.post("/planets", json=test_data)
+
+def test_create_one_planet_no_description(client):
+    # Arrange
+    test_data = {"name": "Pluto",
+                "type": "Ice Giant"}
+
+    # Act & Assert
+    with pytest.raises(KeyError, match = 'description'):
+        response = client.post("/planets", json=test_data)
+
+def test_create_one_planet_with_extra_keys(client, two_saved_planets):
+    # Arrange
+    test_data = {
+        "extra": "some stuff",
+        "name": "Pluto",
+        "description": "The Best!",
+        "type": "Gas Giant",
+        "another": "last value"
+    }
+
+    # Act
+    response = client.post("/planets", json=test_data)
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 201
+    assert response_body == "Planet Pluto successfully created"
