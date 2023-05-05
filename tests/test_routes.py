8ea92744fde90edffc29b46413a0fa20b1fe9297
louloera <louloera@gmail.com>
2023-05-05 14:30:41 -0700

    Created test case to validate the get rout for all planets

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -7,7 +7,7 @@ def test_get_all_planets_with_no_records(client):
     assert response.status_code == 200
     assert response_body == []
 
-def test_get_all_planets_with_two_records(client, two_planets):
+def test_get_one_planet_with_two_records(client, two_planets):
     # Act
     response = client.get("/planets/1")
     response_body = response.get_json()
@@ -26,3 +26,12 @@ def test_empty_database_returns_404(client):
     #assert   
     assert response.status_code == 404
     assert response_body == {"message":"planet 1 not found"}
+
+def test_get_planets_with_two_records(client, two_planets):
+    # Act
+    response = client.get("/planets")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 200
+    assert response_body == [{"id": 1,"name":"Venus","description":"Gassy love goddess","place":2},{"id" : 2, "name":"Earth","description" : "le home", "place": 3}]  
