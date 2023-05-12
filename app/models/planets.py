7eb8338e9264d2be712ad5ad4f9fc7c56565dc52
Paige <ph.paigee@gmail.com>
2023-05-12 13:29:38 -0400

    completed waves 01 and 02

--- app/models/planets.py
+++ app/models/planets.py
@@ -1,6 +1,22 @@
 from app import db
 
-class Planets(db.Model):
-    id = int
-    name = "Saturn"
-    description = "big planet"
+class Planets():
+    def __init__(self, id, name, description):
+        self.id = id
+        self.name = name
+        self.description = description
+    
+    def to_dict(self):
+        planet_as_dict = {}
+        planet_as_dict["id"] = self.id
+        planet_as_dict["name"] = self.name
+        planet_as_dict["description"] = self.description
+
+        return planet_as_dict
+
+
+# planets = [
+#     Planets(1, "Saturn", "Gaseous planet with rings"),
+#     Planets(2, "Jupiter", "Strom planet"),
+#     Planets(3, "Venus", "Where girls come from")
+# ]
--- app/routes.py
+++ app/routes.py
@@ -1,17 +1,29 @@
 from flask import Blueprint, jsonify, abort, make_response, request
+from app.models.planets import *
 
 
 planets_bp = Blueprint("planets_bp", __name__, url_prefix="/planets")
 
-@planets_bp.route("", methods=["POST"])
-def create_planets():
-    request_body = request.get_json()
-    new_planet = 
+# def validate_planet(planet_id):
+#     try:
+#         planet_id = int(planet_id)
+#     except:
+#         abort(make_response({"message":f"planet {planet_id} invalid"}, 400))
+    
+#     for planet in planets:
+#         if planet_id == planet.id:
+#             return planet
+#     abort(make_response({"message":f"planet {planet_id} not found"}, 404))
 
-@planets_bp.route("", methods=["GET"])
-def read_all_planets():
-    planets_response = []
+# @planets_bp.route("", methods=["GET"])
+# def read_all_planets():
+#     planets_response = []
+#     # for planet in planets:
+#     #     planets_response.append(planet.to_dict())
+#     return jsonify(planets_response)
 
-    for planet in planets:
-        planets_response.append(planet.to_dict())
-    return jsonify(planets_response)
+# @planets_bp.route("/<planet_id>", methods=["GET"])
+# def read_one_planet(planet_id):
+#     planet = validate_planet(planet_id)
+#     return planet.to_dict()
+
